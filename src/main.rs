1494c7ba640a501a44c26a7c7a5562f8318a99c6
Yigit Demirbas <yigit.demirbas@gmail.com>
2022-10-04 14:44:49 +0200

    introduce database

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use cnrust::run;
+use cnrust::configuration::get_configuration;
+use cnrust::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres!");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
