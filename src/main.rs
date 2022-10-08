4b1c2b26b4985478939673b9883a4f4a1289fbe6
Yigit Demirbas <yigit.demirbas@gmail.com>
2022-10-08 22:48:01 +0200

    digital ocean spec

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use cnrust::configuration::get_configuration;
 use cnrust::startup::run;
 use cnrust::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
+
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
@@ -13,8 +13,8 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres!");
+        .connect_lazy_with(configuration.database.with_db());
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
